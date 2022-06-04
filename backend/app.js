38344c9d97ba89f00f41b60b0213deaa8c5d7d9a
Megakawa <hungpromax11@gmail.com>
2022-06-04 09:22:06 +0700

    fix authmiddleware in video route and pdf route

--- backend/app.js
+++ backend/app.js
@@ -16,13 +16,13 @@ const app = express()
 
 app.use(cors({ origin: "http://localhost:3000", credentials: true }))
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", req.headers.origin);
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, X-IP, Content-Type, Accept");
-  res.header("Access-Control-Allow-Credentials", true)
-  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", req.headers.origin);
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, X-IP, Content-Type, Accept");
+//   res.header("Access-Control-Allow-Credentials", true)
+//   res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+//   next();
+// });
 
 app.set('view engine', 'ejs');
 app.use(express.static("./public"))
--- backend/routes/videoroute.js
+++ backend/routes/videoroute.js
@@ -3,7 +3,7 @@ var router = express.Router()
 const authmiddleware = require("./authmiddleware")
 const videoctrl = require("../controllers/videoctrl")
 
-// router.use(authmiddleware.isAuth)
+router.use(authmiddleware.isAuth)
 
 router.route("/watch/:id")
   .get(videoctrl.getVideoById)
